647e077e3efecf92dcfcd1773ad6178881a01169
goe350171-max <goe350171@gmail.com>
2026-06-28 04:18:55 -0700

    Update buildTokenTransaction.js

--- frontend/src/utils/buildTokenTransaction.js
+++ frontend/src/utils/buildTokenTransaction.js
@@ -1,6 +1,18 @@
 import {
   Transaction,
+  SystemProgram,
+  PublicKey,
+  Keypair,
 } from "@solana/web3.js";
+
+import {
+  TOKEN_PROGRAM_ID,
+  MINT_SIZE,
+  createInitializeMintInstruction,
+  getAssociatedTokenAddressSync,
+  createAssociatedTokenAccountInstruction,
+  createMintToInstruction,
+} from "@solana/spl-token";
 
 export async function buildTokenTransaction({
   connection,
